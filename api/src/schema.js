8b089e36611ef21d4cd6583ee7f0c4ba58e6d1ca
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-04-05 19:24:41 +0530

    writing typeDefs
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,21 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  type Query {
+    pets: [Pet]!
+  }
 
 `;
 
