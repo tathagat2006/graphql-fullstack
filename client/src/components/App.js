f9e76fdc3d459ab6f767c85a3babeaa80f4332df
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-04-06 19:30:30 +0530

    client init
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import { Switch, Route } from 'react-router-dom'
+import React, { Fragment } from 'react'
+import Header from './Header'
+import Pets from '../pages/Pets'
+
+const App = () => (
+  <Fragment>
+    <Header />
+    <div>
+      "Hello World"
+    </div>
+  </Fragment>
+)
+
+export default App
